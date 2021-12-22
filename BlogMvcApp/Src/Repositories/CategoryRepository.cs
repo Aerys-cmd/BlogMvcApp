a16b4a62efa7faabb24dcb0afdc535fb9ab20a90
aslisutanrikulu <sutanrikuluasli@gmail.com>
2021-12-22 20:12:27 +0300

    category repository eklendi.

---
+++ BlogMvcApp/Src/Repositories/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using BlogMvcApp.Src.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogMvcApp.Src.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Find(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlogMvcApp/Src/Repositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using BlogMvcApp.Src.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogMvcApp.Src.Repositories
+{
+    interface ICategoryRepository:IRepository<Category>
+    {
+    }
+}
